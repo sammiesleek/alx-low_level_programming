73c43b783992d33c2d1f6d70a7d3b0c77a024031
sammiesleek <samlocomotive1@gmail.com>
2022-10-14 14:01:38 -0700

    ....

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,15 +7,10 @@
  * Return: 0
  */
 int main(void)
-
 {
-
-		int num;
-
-
+int num;
 for (num = 0; num < 10; num++)
-  putchar((num % 10) + '0'); 
+putchar((num % 10) + '0'); 
 putchar('\n');
 return (0);
-
 }
