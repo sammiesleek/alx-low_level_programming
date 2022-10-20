0cb7d7b41d2d88e7511223841f6d0d77b29e3f58
sammiesleek <samlocomotive1@gmail.com>
2022-10-20 12:30:09 -0700

    ..

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * print_numbers -  checks for checks for a digit (0 through 9).
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * print_most_numbers - a function that prints the numbers, from 0 to 9
+ *
+ * Return: 0-9, excluding 2 and 4, followed by new line
+ */
+void print_most_numbers(void)
+{
+	int h;
+
+	for (h = 0; h <= 9; h++)
+	{
+		if ((h == 2) || (h == 4))
+			continue;
+		else
+			putchar(h + '0');
+	}
+	 putchar('\n');
+}
